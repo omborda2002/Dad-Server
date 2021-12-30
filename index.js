be884f695b2c3cacbaa83e29b118f561656a40d8
Om_Borda <omborda2002@gmail.com>
2021-12-30 19:31:19 +0530

    ALL GOOOOOOOOOOOOOOD.

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 const data = require("./data.json");
+let bigImg = require("./bigImg.json");
+
 const port = 3000;
 
 // console.log(data);
@@ -16,14 +18,70 @@ let arr = [];
 app.get("/", (req, res) => {
   //   res.send('Hello World!');
   // console.log(data)
-  res.render("home", { data });
+  let num = 0;
+  res.render("home", { data, bigImg, num });
 });
 app.get("/home", (req, res) => {
   //   res.send('Hello World!');
+  // console.log(bigImg[0]);
+  let num = 0;
   res.redirect("/");
 });
+
+app.get("/page/:num", (req, res) => {
+  let { num } = req.params;
+  num = parseInt(num);
+  if (num == 0) {
+    res.redirect("/");
+  } else {
+    res.render("home", { num, data, bigImg });
+  }
+});
+
+//FIXME:
+app.get("/category/:sub", (req, res) => {
+  let { sub } = req.params;
+  arr = [];
+
+  if (sub == "web") {
+    arr = [];
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].type == "Web") {
+        arr.push(data[i]);
+      } else if (data[i].type == "web") {
+        arr.push(data[i]);
+      }
+    }
+  }
+
+  if (sub == "bollywood") {
+    arr = [];
+
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].type == "bollywood") {
+        arr.push(data[i]);
+      } else if (data[i].type == "Bollywood") {
+        arr.push(data[i]);
+      }
+    }
+  }
+  if (sub == "hollywood") {
+    arr = [];
+
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].type == "hollywood") {
+        arr.push(data[i]);
+      } else if (data[i].type == "Hollywood") {
+        arr.push(data[i]);
+      }
+    }
+  }
+
+  res.render("search", { arr });
+});
+
 app.get("/movie/:name", (req, res) => {
-  let datam,type;
+  let datam, type;
   const { name } = req.params;
   // console.log(name);
   for (let i = 0; i < data.length; i++) {
@@ -32,35 +90,42 @@ app.get("/movie/:name", (req, res) => {
       break;
     }
   }
-  if (datam.type == "hollywood" || datam.type == "bollywood" ||datam.type == "Hollywood" ||datam.type =="Bollywood")  {
+  if (
+    datam.type == "hollywood" ||
+    datam.type == "bollywood" ||
+    datam.type == "Hollywood" ||
+    datam.type == "Bollywood"
+  ) {
     type = "movie";
   } else {
-    type ="web";
+    type = "web";
   }
-  res.render("inner", { datam ,data,type});
+  res.render("inner", { datam, data, type, bigImg });
 });
-// app.get("/search", (req, res) => {
-//   const { q } = req.query;
-//   let str = q.toLowerCase();
-//   arr = [];
-//   for (let i = 0; i < data.length; i++) {
-//     let main = data[i].name.toLowerCase().split(" ").join("");
-//     let lm = main.length;
-//     let ls = str.length;
-//     let c = 0;
 
-//     for (let j = 0; j < lm; j++) {
-//       let temp = main.substring(j, ls + j);
-//       if (temp == str) {
-//         c++;
-//       }
-//     }
-//     if (c >= 1) {
-//       arr.push(data[i]);
-//     }
-//   }
-//   res.render("s", { arr });
-// });
+app.get("/search", (req, res) => {
+  const { q } = req.query;
+  let str = q.toLowerCase();
+  arr = [];
+  for (let i = 0; i < data.length; i++) {
+    let main = data[i].name.toLowerCase().split(" ").join("");
+    let lm = main.length;
+    let ls = str.length;
+    let c = 0;
+
+    for (let j = 0; j < lm; j++) {
+      let temp = main.substring(j, ls + j);
+      if (temp == str) {
+        c++;
+      }
+    }
+    if (c >= 1) {
+      arr.push(data[i]);
+    }
+  }
+
+  res.render("search", { arr });
+});
 app.get("*", (req, res) => {
   res.send("NOT FOUND");
 });
